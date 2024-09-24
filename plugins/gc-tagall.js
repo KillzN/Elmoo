593b9831844072aee2504a64f6087cc669acb4d2
KillzN <scorpionsheep203@gmail.com>
2024-09-23 23:08:44 -0300

    KILL - BOT

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,26 +1,19 @@
-const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.gc_tagall
-
-  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
   if (!(isAdmin || isOwner)) {
-    global.dfail('admin', m, conn);
-    throw false;
+  global.dfail('admin', m, conn)
+  throw false
   }
-  const pesan = args.join` `;
-  const oi = `*NikolBot Te menciona  ✍* ${pesan}`;
-  let teks = `*⺀ꨄ 𝐌𝐄𝐍𝐂𝐈𝐎𝐍 𝐆𝐄𝐍𝐄𝐑𝐀𝐋 ꨄ⺀*\n\n ${oi}\n\n➥ _*╭ ⊹✨ 𝙼𝙴𝙽𝚂𝙰𝙹𝙴: *_\n`;
-  for (const mem of participants) {
-    teks += `┊ᡣ𐭩 @${mem.id.split('@')[0]}\n`;
+  let pesan = args.join` `
+  let oi = `*𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝘼𝙎:* ${pesan}`
+  let teks = `*🎀 𝗕𝗮𝗿𝗯𝗶𝗲 𝘃𝘀 𝗺𝗮𝗹𝗮𝘀 𝘆 𝗮𝘀𝗶́.*\n\n ${oi}\n\n➥ _*𝗕𝗮𝗿𝗯𝗶𝗰𝗶𝘁𝗮 𝗶𝗻𝘃𝗼𝗰𝗮𝗻𝗱𝗼 𝗮 𝗹@𝘀 𝗳𝗹𝗼𝗷@𝘀 𝗹𝗲𝘃𝗮𝗻𝘁𝗲𝗻 𝗰𝘂𝗹𝗼𝘀 𝘃𝗮𝗴𝗼𝘀:*_\n`
+  for (let mem of participants) {
+  teks += `🥷🏼 @${mem.id.split('@')[0]}\n`}
+  teks += `└ *BᴀʀʙɪᴇBᴏᴛ ⇝@xbarbicita.7*`
+  conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
   }
-  teks += `╰───── 𝐍𝐢𝐤𝐨𝐥 𝐁𝐨𝐭 ─────╯`;
-  conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
-};
-handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
-handler.tags = ['group'];
-handler.command = /^(tagall|invocar|invocacion|todos|invocación|putos)$/i;
-handler.admin = true;
-handler.group = true;
-export default handler;
+  handler.help = ['tagall <mesaje>','invocar <mesaje>']
+  handler.tags = ['group']
+  handler.command = /^(tagall|invocar|invocacion|todos|invocación|aviso|despierten|putos|animales)$/i
+  handler.admin = true
+  handler.group = true
+  export default handler
